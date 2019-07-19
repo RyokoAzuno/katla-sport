b78574d94bc0e57ad664a8ea574b6fbf4b2165b3
Stanislau Kazimirau <stanislau_kazimirau@epam.com>
2019-07-19 21:52:08 +0300

    Merge step13. Add new actions to HiveSectionsController. Add new methods to HiveSectionService.

--- KatlaSport.Services/HiveManagement/HiveManagementMappingProfile.cs
+++ KatlaSport.Services/HiveManagement/HiveManagementMappingProfile.cs
@@ -13,8 +13,12 @@ namespace KatlaSport.Services.HiveManagement
             CreateMap<DataAccessHive, Hive>();
             CreateMap<DataAccessHiveSection, HiveSectionListItem>();
             CreateMap<DataAccessHiveSection, HiveSection>();
+
             CreateMap<UpdateHiveRequest, DataAccessHive>()
                 .ForMember(r => r.LastUpdated, opt => opt.MapFrom(p => DateTime.UtcNow));
+
+            CreateMap<UpdateHiveSectionRequest, DataAccessHiveSection>()
+                .ForMember(r => r.LastUpdated, opt => opt.MapFrom(h => DateTime.UtcNow));
         }
     }
 }
